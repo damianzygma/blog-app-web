b93dcc02c1ac36b59d3bbaa57d00407e15700b23
damianzygma <damian.zygma@gmail.com>
2023-09-04 19:08:01 +0200

    Add new blog web application project

---
+++ src/main/java/com/damianzygma/blogappweb/BlogAppWebApplication.java
@@ -0,0 +1,13 @@
+package com.damianzygma.blogappweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppWebApplication.class, args);
+	}
+
+}
