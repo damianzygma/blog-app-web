ba33c055bac0d1568376436846277e86a8246631
damianzygma <damian.zygma@gmail.com>
2024-02-22 11:21:55 +0100

    feature: Create handler method to save post

--- src/main/java/com/damianzygma/blogappweb/controller/PostController.java
+++ src/main/java/com/damianzygma/blogappweb/controller/PostController.java
@@ -5,6 +5,7 @@ import com.damianzygma.blogappweb.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -34,6 +35,24 @@ public class PostController {
         PostDto postDto = new PostDto();
         model.addAttribute("post", postDto);
         return "admin/create_post";
+    }
+
+
+    // handler method to handle form submit request
+    @PostMapping("/admin/posts")
+    public String createPost(@ModelAttribute PostDto postDto){
+        postDto.setUrl(getUrl(postDto.getTitle()));
+        postService.createPost(postDto);
+        return "redirect:/admin/posts";
+    }
+
+    private static String getUrl(String postTitle){
+        //OOPS Concepts Explained in Java
+        //oops-concepts-explained-in-java
+        String title = postTitle.trim().toLowerCase();
+        String url = title.replaceAll("\\s+", "-");
+        url = url.replaceAll("[^A-Za-z0-9]", "-");
+        return url;
     }
 
 
