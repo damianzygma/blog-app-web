3a3ddd3261a1c4309e83d88b2ef63d7f49032763
damianzygma <damian.zygma@gmail.com>
2024-03-05 16:10:51 +0100

    feature: Create comment form handling

--- src/main/java/com/damianzygma/blogappweb/controller/BlogController.java
+++ src/main/java/com/damianzygma/blogappweb/controller/BlogController.java
@@ -1,5 +1,6 @@
 package com.damianzygma.blogappweb.controller;
 
+import com.damianzygma.blogappweb.dto.CommentDto;
 import com.damianzygma.blogappweb.dto.PostDto;
 import com.damianzygma.blogappweb.service.PostService;
 import org.springframework.stereotype.Controller;
@@ -32,7 +33,9 @@ public class BlogController {
     private String showPost(@PathVariable("postUrl") String postUrl,
                             Model model){
         PostDto post = postService.findPostByUrl(postUrl);
+        CommentDto commentDto = new CommentDto();
         model.addAttribute("post", post);
+        model.addAttribute("comment", commentDto);
         return "blog/blog_post";
     }
 
