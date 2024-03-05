5da8f53700d69f32e15b033ee567d8124ab49a1f
damianzygma <damian.zygma@gmail.com>
2024-03-05 15:29:49 +0100

    feature: Create CommentDto

---
+++ src/main/java/com/damianzygma/blogappweb/dto/CommentDto.java
@@ -0,0 +1,30 @@
+package com.damianzygma.blogappweb.dto;
+
+import com.damianzygma.blogappweb.entity.Post;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentDto {
+
+    private Long id;
+    
+    private String name;
+
+    private String email;
+
+    private String content;
+
+    private LocalDateTime createdOn;
+
+    private LocalDateTime updatedOn;
+
+}
