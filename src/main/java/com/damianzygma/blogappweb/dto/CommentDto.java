09332a3c9a8f32bc9e41f0bbdbcc70aff4f6ee4d
damianzygma <damian.zygma@gmail.com>
2024-03-05 15:40:46 +0100

    feature: Create CommentMapper

--- src/main/java/com/damianzygma/blogappweb/dto/CommentDto.java
+++ src/main/java/com/damianzygma/blogappweb/dto/CommentDto.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 public class CommentDto {
 
     private Long id;
-    
+
     private String name;
 
     private String email;
---
+++ src/main/java/com/damianzygma/blogappweb/mapper/CommentMapper.java
@@ -0,0 +1,32 @@
+package com.damianzygma.blogappweb.mapper;
+
+import com.damianzygma.blogappweb.dto.CommentDto;
+import com.damianzygma.blogappweb.entity.Comment;
+
+public class CommentMapper {
+
+    // map Comment entity to CommentDto
+    public static CommentDto mapToCommentDto(Comment comment){
+        return CommentDto.builder()
+                .id(comment.getId())
+                .name(comment.getName())
+                .email(comment.getEmail())
+                .content(comment.getContent())
+                .createdOn(comment.getCreatedOn())
+                .updatedOn(comment.getUpdatedOn())
+                .build();
+    }
+
+    // map CommentDto to Comment entity
+    public static Comment mapToComment(CommentDto commentDto){
+        return Comment.builder()
+                .id(commentDto.getId())
+                .name(commentDto.getName())
+                .email(commentDto.getEmail())
+                .content(commentDto.getContent())
+                .createdOn(commentDto.getCreatedOn())
+                .updatedOn(commentDto.getUpdatedOn())
+                .build();
+    }
+
+}
